ccb7459501ba6e8d4ea1192ebec9cbd32801152c
khy0425 <from_ap@naver.com>
2021-05-06 22:26:30 +0900

    게시판 생성

--- fc_community/board/views.py
+++ fc_community/board/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render
-from .models import Fcuser
-# Create your views here.
+from .models import Board
 
 
-def register(request):
-    if request.method == 'GET':
-        return render(request, 'register.html')
-    elif request.method == 'POST':
-        username = request.POST('username')
-        password = request.POST('password')
-        re_password = request.POST('re_password')
-
-        fcuser = Fcuser(
-            username=username,
-            password=password
-        )
-
-        fcuser.save()
-
-        return render(request, 'register.html')
+def board_list(request):
+    boards = Board.objects.all().order_by('-id')
+    return render(request, 'board_list.html', {'boards': boards})
